f0f474b30ab98dcaf0d4af89f7278e4d90f11110
David <davidbalyan9@gmail.com>
2021-03-13 21:32:37 +0200

    Added functionality

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 let canvas = document.getElementById("canvas");
-let newDiv;
+let newDivArr = [];
+let btn = document.getElementById("reset-btn");
 
 function makeDivs() {
     for (let i = 0; i < 10000; i++) {
-        newDiv = document.createElement("div");
-        newDiv.classList.add("tile");
-        canvas.appendChild(newDiv);
+        newDivArr[i] = document.createElement("div");
+        newDivArr[i].classList.add("tile");
+        canvas.appendChild(newDivArr[i]);
     }
-}
 
-function changeBg() {
-    $(".tile")
+    newDivArr.forEach(div => {
+        div.addEventListener("click", () => {
+            newDivArr.forEach(tile => {
+                tile.addEventListener("mouseover", () => {
+                    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+                    tile.style.backgroundColor = '#' + n.slice(0, 6);
+                });
+            });
+        });
+    });
+
+    btn.addEventListener("click", () => {
+        newDivArr.forEach(item => {
+            item.style.backgroundColor = "transparent";
+        });
+    });
 }
 
 makeDivs();
