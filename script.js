fd32e95dfc00a0097efcc7485b5aea72156df87b
David <davidbalyan9@gmail.com>
2021-03-19 19:40:11 +0200

    fixed responsiveness

--- script.js
+++ script.js
@@ -5,8 +5,15 @@ let divs = [];
 let sizeBtn = document.getElementById('grid-btn');
 let resetBtn = document.getElementById('reset-btn');
 let input = document.getElementById('size');
+let body = document.getElementById('body');
 
 function main() {
+    if (window.innerWidth < 600) {
+        body.style.fontSize = '75%';
+        canvas.style.width = '200px';
+        canvas.style.height = '200px';
+    }
+
     ////////////default size///////////
     canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
